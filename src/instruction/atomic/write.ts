7776b435683afdeb40aea28d9306b29e3ad9126e
Andreas Krühlmann <andreas@kruhlmann.dev>
2023-08-22 16:42:39 +0200

    Add preliminary printx

--- src/instruction/atomic/write.ts
+++ src/instruction/atomic/write.ts
@@ -2,7 +2,7 @@ import { Instruction } from "../instruction";
 
 export class WriteMemoryInstruction extends Instruction {
     public to_asm(_instruction_index: number): string {
-        return "pop rbx\npop rax\nmov [rax], bl";
+        return "pop rbx\npop rax\nmov [rax], rbx";
     }
     public to_wat(_instruction_index: number): string {
         throw new Error("Method not implemented.");
